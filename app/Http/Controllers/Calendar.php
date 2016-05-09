01846d552027712f80b75e86c4c114a03ebccdde
Fintan MacMahon <fintan.macmahon@gmail.com>
2016-05-09 11:46:06 +0100

    Removed uneeded methods.

--- app/Http/Controllers/Calendar.php
+++ app/Http/Controllers/Calendar.php
@@ -2,8 +2,6 @@
 
 namespace HeritageCalendar\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class Calendar extends Controller
 {
     /**
@@ -16,28 +14,6 @@ class Calendar extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param \Illuminate\Http\Request $request
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
      * Display the specified resource.
      *
      * @param int $id
@@ -45,43 +21,6 @@ class Calendar extends Controller
      * @return \Illuminate\Http\Response
      */
     public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param int $id
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @param int                      $id
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param int $id
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
     {
         //
     }
